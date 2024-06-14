02cd7f04c950aaa927bd90ab62f78f995f7d04ce
JJangJJun <jhjang888@naver.com>
2024-06-15 02:33:55 +0900

    2024/06/15/02:32
    stage1 -> stage2

---
+++ 3D_Game_jang/Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Weapon };
+    public Type type;
+    public int value;
+
+    private Rigidbody rigid; // 리지드바디 컴포넌트
+    private SphereCollider sphereCollider; // 스피어 콜라이더 컴포넌트
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * 10 * Time.deltaTime); // 아이템 회전
+    }
+}
